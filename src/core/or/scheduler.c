c3a90df0374ffe9b57f6c9bd59d3ee488c7bfd56
Matt Traudt <sirmatt@ksu.edu>
2019-03-20 09:27:54 -0400

    Log sndbuflen and rcvbuflen

--- src/core/or/scheduler.c
+++ src/core/or/scheduler.c
@@ -282,18 +282,25 @@ scheduler_evt_callback(mainloop_event_t *event, void *arg)
     uint32_t sndqlen = 0;
     uint32_t notsent = 0;
     uint32_t rcvqlen = 0;
+    int sndbuflen = 0;
+    int rcvbuflen = 0;
+    socklen_t optlen = sizeof(sndbufflen);
     struct tcp_info tcp;
     socklen_t tcp_info_len = sizeof(tcp);
     tor_assert(getsockopt(sock, SOL_TCP, TCP_INFO, (void *)&(tcp), &tcp_info_len) >= 0);
+    tor_assert(getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sndbuflen, &optlen) >= 0);
+    tor_assert(getsockopt(sock, SOL_SOCKET, SO_RCVBUF, &rcvbuflen, &optlen) >= 0);
     tor_assert(ioctl(sock, SIOCOUTQNSD, &notsent) >= 0);
     tor_assert(ioctl(sock, SIOCOUTQ, &sndqlen) >= 0);
     tor_assert(ioctl(sock, SIOCINQ, &rcvqlen) >= 0);
     log_info(
         LD_SCHED, "%lu %s sock=%i cwnd=%u unacked=%u sndqlen=%u notsent=%u "
-        "outbuf=%u cmux=%u cmux_destroy=%u ewma_cell_count=%f rcvqlen=%u",
+        "outbuf=%u cmux=%u cmux_destroy=%u ewma_cell_count=%f rcvqlen=%u "
+        "sndbuflen=%d rcvbuflen=%d",
         t, get_scheduler_type_string(sched->type),
         sock, tcp.tcpi_snd_cwnd, tcp.tcpi_unacked, sndqlen, notsent,
-        outbuf, cmux_regular, cmux_destroy, ewma_cell_count, rcvqlen);
+        outbuf, cmux_regular, cmux_destroy, ewma_cell_count, rcvqlen,
+        sndbuflen, rcvbuflen);
   } SMARTLIST_FOREACH_END(chan);
 #endif /* HAVE_KIST_SUPPORT */
 
